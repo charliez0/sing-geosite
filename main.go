f8ce7b79e3381c64529006af7d51c80de613a873
charliez0 <charliez0sp@gmail.com>
2024-02-13 11:34:13 +0800

    Fixes tag

--- main.go
+++ main.go
@@ -374,7 +374,7 @@ func release(source string, destination string, output string, cnOutput string, 
 	if err != nil {
 		return err
 	}
-	setActionOutput("tag", *sourceRelease.Name)
+	setActionOutput("tag", *sourceRelease.TagName)
 	return nil
 }
 
